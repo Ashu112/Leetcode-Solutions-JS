e19f55eb942680b3f0c3e5969a41f92ce2e4719a
Ashutosh Kumar <kashu2611@gmail.com>
2026-02-23 19:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 58.2 MB (49.77%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    // 1. binary search implementation
+    let l = 0; 
+    let r = nums.length - 1;
+    while(l<=r){
+        let mid = Math.floor((l+r)/2);
+        if(nums[mid] === target)return mid;
+        else if(target < nums[mid]){
+            r = mid -1;
+        }else if(target > nums[mid]){
+            l = mid+1;
+        }
+    }
+    return -1;
+};
